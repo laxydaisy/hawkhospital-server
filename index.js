9982f4d5a1dbd4f329e7ac1d8a146625488277dd
unknown <toroitichdaisy@gmail.com>
2019-06-11 12:51:14 +0300

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const mysql = require("mysql")
 const app = express();
 
-app.get("/",function (req, res){
-    //logic
-    res.send("message", "Hello World!");
+const pool = mysql.createPool({
+    host: "localhost",
+    user: "foo",
+    password: "password",
+    database: "hawkhospital",
+    insecureAuth: true
+});
+app.get("/api/hawkhospital", (req, res) => {
+    pool.query("SELECT * from doctor", (error, rows) => {
+        if (error) {
+            return res.status(500).json({ error });
+
+        }
+        res.json(rows);
+    });
 });
 
-app.listen(8081,function(){
+app.listen(8081, function () {
     console.log("App listening on port 8081");
 });
