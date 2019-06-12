88af205e0f2fb93bdedf93eb70a94f45b0e24c38
unknown <toroitichdaisy@gmail.com>
2019-06-12 12:20:55 +0300

    query codes

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const mysql = require("mysql")
 const app = express();
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "foo",
-    password: "password",
-    database: "hawkhospital",
-    insecureAuth: true
+    host: process.env.DB_HOST,
+     user: process.env.DB_USER,
+     password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 app.get("/api/hawkhospital", (req, res) => {
     pool.query("SELECT * from doctor", (error, rows) => {
@@ -18,6 +17,19 @@ app.get("/api/hawkhospital", (req, res) => {
         res.json(rows);
     });
 });
+app.get("/api/hawkhospital/:id", (req, res) => {
+         pool.query(
+             "SELECT id, name FROM patient WHERE id = ?",
+             [req.params.id],
+             (error, rows) => {
+                 if (error) {
+                     return res.status(500).json({ error });
+                 }
+    
+                 res.json(rows);
+             }
+         );
+     });
 
 app.listen(8081, function () {
     console.log("App listening on port 8081");
