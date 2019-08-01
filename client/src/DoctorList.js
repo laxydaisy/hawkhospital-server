591429a95113dc6a093d9b477591f02523f5273f
leazylaxy <toroitichdaisy@gmail.com>
2019-08-01 18:06:58 +0300

    changes

--- client/src/DoctorList.js
+++ client/src/DoctorList.js
@@ -53,18 +53,10 @@ fetchHomepageinfo();{
                     loading: false,
                     error: true
                 });
-            });
-        }
-        toDateString(dateTime); {
-            const date = new Date(dateTime);
-            const year = date.getFullYear();
-            const month = date.getMonth() + 1;
-          const day = date.getDate();
             
-         return `${year}-${month}-${day}`;
-        }
- 
-     }  
+               });
+            }
+    }  
         
 }
 
